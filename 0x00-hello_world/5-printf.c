7fa7295760e45d89adfb6a89bcfc328838f9e559
thejosh8 <irumvajosh7@gmail.com>
2023-02-16 18:36:36 -0800

    task4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio>
+
+/**
+ * main - print the code using printf
+ *
+ * return: 0
+ */
+int main (void)
+{
+	printf ("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
