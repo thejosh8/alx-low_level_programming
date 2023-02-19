64e2c1609378054cdf6f35fd69011e569233f600
thejosh8 <irumvajosh7@gmail.com>
2023-02-19 06:49:58 -0800

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Return: 0 If successful
+*
+* Description: Bla Bla Bla
+*/
+int main(void)
+{
+int capital;
+int lower;
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+	putchar(lower);
+}
+for (capital = 'A'; capital <= 'Z'; capital++)
+{
+putchar(capital);
+}
+printf("\n");
+return (0);
+}
