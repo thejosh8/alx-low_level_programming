97b18847b7210a25990f8e272878e67f6dd26f20
thejosh8 <irumvajosh7@gmail.com>
2023-02-28 07:13:26 -0800

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,33 @@
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+	while (s[c])
 	{
-
-	if (s[i] >= '0' && s[i] <= '9')
+	if (s[c] == 45)
 	{
-		digit = s[i] - '0';
-		if (d % 2)
-			digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
-		if (s[i + 1] < '0' || s[i + 1] > '9')
-			break;
-		f = 0;
+	min *= -1;
 	}
-	i++;
-}
-if (f == 0)
-return (0);
-return (n);
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
 }
